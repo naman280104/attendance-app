2bde6f885955672f24fd044080464a1825c542e0
Naman Goyal <naman280104@gmail.com>
2024-02-24 09:50:24 +0530

    server directory structure defined

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+const PORT= process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port',PORT);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
