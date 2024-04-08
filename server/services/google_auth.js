29f362cf42347bd18ca180c0ee6963978c3913a6
Naman Goyal <naman280104@gmail.com>
2024-04-08 19:51:14 +0530

    auth modified to work for both platforms, ios and android

--- server/services/google_auth.js
+++ server/services/google_auth.js
@@ -1,11 +1,13 @@
 const { OAuth2Client } = require('google-auth-library');
 const axios = require('axios');
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const client = new OAuth2Client(CLIENT_ID);
+const CLIENT_ID_ANDROID = process.env.GOOGLE_CLIENT_ID_ANDROID;
+const CLIENT_ID_IOS = process.env.GOOGLE_CLIENT_ID_IOS;
+const client_ANDROID = new OAuth2Client(CLIENT_ID_ANDROID);
+const client_IOS = new OAuth2Client(CLIENT_ID_IOS);
 
 
-const verifyGoogleToken = async (accessToken) => {
+const verifyGoogleToken = async (accessToken,CLIENT_ID) => {
     console.log(accessToken);
     const tokenInfoEndpoint = `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${accessToken}`;
     const userInfoEndpoint = `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`;
@@ -15,9 +17,9 @@ const verifyGoogleToken = async (accessToken) => {
         const responseuser = await axios.get(userInfoEndpoint);
         const tokenInfo = responsetoken.data;
         console.log(tokenInfo);
-        if (tokenInfo.aud === CLIENT_ID) {  
+        if (tokenInfo.aud === CLIENT_ID_ANDROID || tokenInfo.aud === CLIENT_ID_IOS) {  
             return responseuser.data;
-        } 
+        }
         else {
             console.log('Access token is not valid for your client.');
             throw new Error('Access token is not valid for your client.');
